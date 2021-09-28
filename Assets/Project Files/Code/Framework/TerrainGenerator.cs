e59e3cde38e4b2c146ca72b5e0c5bb35aee83c47
iswanlun <iswanlun@asu.edu>
2021-09-28 16:03:43 -0700

    Added terrain motion structure

--- Assets/Project Files/Code/Framework/TerrainGenerator.cs
+++ Assets/Project Files/Code/Framework/TerrainGenerator.cs
@@ -8,25 +8,56 @@ public class TerrainGenerator : MonoBehaviour
     public int width = 500; // x
     public int length = 500; // z
     public int depth = 20; // y
+    public int perlin_octaves = 3;
 
     public float lacunarity;
     public float persistance;
 
+    private float satillite_offset, satillite_speed;
+    private bool ascending;
+
     public float tempScale = 20f;
+
+    private float[,] terrain_heights;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        satillite_offset = 0;
+        satillite_speed = 5f;
+        ascending = true;
+
         Terrain terrain = GetComponent<Terrain>();
         terrain.terrainData = GetTerrainData( terrain.terrainData );
+    }
+
+        // Update is called once per frame
+    void Update() {
+
+        if ( ascending ) {
+            satillite_offset += Time.deltaTime * satillite_speed;
+        } else {
+            satillite_offset -= Time.deltaTime * satillite_speed;
+        }
+
+        if ( satillite_offset > 99999 ) {
+            ascending = false;
+        }
+        if ( satillite_offset <= 0 ) {
+            ascending = true;
+        }
+
+        terrain_heights = UpdateHeights( terrain_heights );
     }
 
     TerrainData GetTerrainData( TerrainData terrainData ) {
 
         terrainData.heightmapResolution = width + 1;
+        terrain_heights = GenerateHeights();
 
         terrainData.size = new Vector3( width, depth, length );
-        terrainData.SetHeights(0, 0, GenerateHeights());
+        terrainData.SetHeights(0, 0, terrain_heights);
         return terrainData;
     }
 
@@ -38,20 +69,40 @@ public class TerrainGenerator : MonoBehaviour
 
             for ( int z = 0; z < length; ++z ) {
 
-                heights[x,z] = PerlinCalculator(x, z);
+                heights[x,z] = PerlinCalculator( x, z, perlin_octaves );
             }
         }
         return heights;
     }
 
-    float PerlinCalculator( int x, int z ) {
+    float PerlinCalculator( int x, int z, int octaves ) {
 
-        float xCoord = (float)x / width * tempScale;
-        float zCoord = (float)z / width * tempScale;
+        float xCoord = (float)x / width * tempScale + satillite_offset;
+        float zCoord = (float)z / width * tempScale + satillite_offset;
 
         return Mathf.PerlinNoise( xCoord, zCoord );
     }
 
-    // Update is called once per frame
-    void Update() {}
+
+
+    float[,] UpdateHeights( float[,] heights ) {
+
+
+        for ( int x = 0; x < width; ++x ) {
+
+            for ( int z = 0; z < length-1; ++z ) {
+
+                heights[x, z] = heights[x,z+1];
+            }
+
+        }
+
+        for ( int x = 0; x < width; ++x ) {
+
+            heights[x, length-1] = PerlinCalculator(x, length-1, perlin_octaves );
+
+        }
+
+        return heights;
+    }
 }
