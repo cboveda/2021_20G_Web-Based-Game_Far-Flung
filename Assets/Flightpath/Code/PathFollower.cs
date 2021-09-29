7bf89d6900269588bf175095160ea43e0fb831b0
Chris Boveda <cboveda@asu.edu>
2021-09-29 12:26:19 -0700

    Update pathfollower to allow for speed adjustment

--- Assets/Flightpath/Code/PathFollower.cs
+++ Assets/Flightpath/Code/PathFollower.cs
@@ -6,10 +6,9 @@ public class PathFollower : MonoBehaviour
 {
     public Transform path;
     private float t;
+    public float speed;
     private Vector2 position;
     private bool unlocked;
-
-    public Transform Path { get => path; set => path = value; }
 
     // Start is called before the first frame update
     void Start()
@@ -22,29 +21,36 @@ public class PathFollower : MonoBehaviour
     void Update()
     {
         if (unlocked) {
-            StartCoroutine(Move(Path));
+            StartCoroutine(Move());
         }
     }
 
-    private IEnumerator Move(Transform path) {
+    private IEnumerator Move() {
+        // Prevent other Coroutines from starting.
+        unlocked = false;
+
         Vector2 start = path.GetChild(0).position;
         Vector2 startDir = path.GetChild(1).position;
         Vector2 end = path.GetChild(2).position;
         Vector2 endDir = path.GetChild(3).position;
 
         while (t < 1) {
-            t += Time.deltaTime;
+            t += Time.deltaTime * speed;
 
+            // Determine and set new position
             position = Mathf.Pow(1-t, 3) * start +
                 3 * Mathf.Pow(1-t, 2) * t * startDir +
                 3 * (1-t) * Mathf.Pow(t, 2) * endDir +
                 Mathf.Pow(t, 3) * end;
-
             transform.position = position;
-
+            
+            //Sync with framerate
             yield return new WaitForEndOfFrame();
         }
-
+    
         t = 0f;
+
+        // Release lock on Coroutines.
+        unlocked = true;
     }
 }
