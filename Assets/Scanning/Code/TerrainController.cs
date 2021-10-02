dcaf95c190671348026a02475d5aad58ec655098
iswanlun <iswanlun@asu.edu>
2021-10-01 19:03:36 -0700

    Recreating perlin noise generator to be satilite transform specific

---
+++ Assets/Scanning/Code/TerrainController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainController : MonoBehaviour
+{
+
+    public const float maxViewDst = 300f;
+    public Transform viewer;
+    public static Vector2 viewerPosition;
+    int chunksize, chunksVisibleToViewer;
+
+    void Start() {
+
+        
+
+
+    } 
+    
+
+
+}
