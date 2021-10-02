339f51be7a5446c7102a2f395462fd0186804902
iswanlun <iswanlun@asu.edu>
2021-10-02 16:29:12 -0700

    Added renderer for terrain texture

--- Assets/Scanning/Code/TerrainController.cs
+++ Assets/Scanning/Code/TerrainController.cs
@@ -7,10 +7,18 @@ public class TerrainController : MonoBehaviour
     public MeshFilter meshFilter;
     public MeshRenderer meshRenderer;
 
+    public int chunkDim = 241;
+    public Color[] terrainGradient = new Color[2];
+
     void Start() {
 
-        float[,] terrain = TerrainGenerator.GetTerrainHeights(0, 0, 241);
+        float[,] terrain = TerrainGenerator.GetTerrainHeights(0, 0, chunkDim);
         meshFilter.sharedMesh = MeshGenerator.GenerateTerrainMesh(terrain, 50).CreateMesh();
+
+        // fetch a texture for mesh
+        Texture2D texture = TerrainTextureGenerator.CreateTexture(terrainGradient, terrain, chunkDim, chunkDim);
+        meshRenderer.sharedMaterial.mainTexture = texture;
+        //meshRenderer.transform.localScale = new Vector3(chunkDim, 1, chunkDim); // might interfere with height collisions, can restrict
 
     }
 }
