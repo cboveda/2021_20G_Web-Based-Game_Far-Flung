b5d9379690a1686c20a07dd8475d351a56a7e0e9
iswanlun <iswanlun@asu.edu>
2021-10-14 11:41:35 -0700

    Update camera follower and flight control to be in line with orbital pattern

--- Assets/Scanning/Code/FlightControl.cs
+++ Assets/Scanning/Code/FlightControl.cs
@@ -8,20 +8,48 @@ public class FlightControl : MonoBehaviour
     public float pitchSpeed = 100f;
     public float maxAltitude = 500f;
 
-    float pitch, roll;
+    [Range(0,1)]
+    public float hozSlerpSpped;
+    [Range(0,1)]
+    public float vertSlerpSpeed;
+
+    public Vector3 rlTranslate = new Vector3(10, 0, 0);
+
+    Quaternion bankRight = Quaternion.Euler(0, 30, -30);
+    Quaternion bankLeft = Quaternion.Euler(0, -30, 30);
+    Quaternion noseUp = Quaternion.Euler(-30, 0, 0);
+    Quaternion noseDown = Quaternion.Euler(30, 0, 0);
+    Quaternion noQuat = Quaternion.Euler(0, 0, 0);
+    
+    public float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+
 
     void Update() {
 
-        roll = Input.GetAxisRaw("Horizontal");
-        pitch = Input.GetAxisRaw("Vertical");
+        float roll  = Input.GetAxis("Horizontal");
+        float pitch = Input.GetAxis("Vertical");
 
-        transform.Rotate( Vector3.back * roll * rollSpeed * Time.deltaTime, Space.Self );
-        transform.Rotate( Vector3.right * pitch * pitchSpeed * Time.deltaTime, Space.Self );
-
-        if ( Input.GetKey("space") ) {
-            transform.Translate( Vector3.forward * speed * Time.deltaTime );
+        if ( roll > 0 ) {
+            transform.rotation = Quaternion.Slerp( transform.rotation, bankRight, hozSlerpSpped );
+            transform.position = Vector3.SmoothDamp(transform.position, transform.position + rlTranslate, ref velocity, smoothTime);
+        } else if ( roll < 0 ) {
+            transform.rotation = Quaternion.Slerp( transform.rotation, bankLeft, hozSlerpSpped );
+            transform.position = Vector3.SmoothDamp(transform.position, transform.position - rlTranslate, ref velocity, smoothTime);
+        } else {
+            transform.rotation = Quaternion.Slerp( transform.rotation, noQuat, hozSlerpSpped );
         }
 
+        if ( pitch > 0 ) {
+            transform.rotation = Quaternion.Slerp( transform.rotation, noseUp, vertSlerpSpeed );
+        } else if ( pitch < 0 ) {
+            transform.rotation = Quaternion.Slerp( transform.rotation, noseDown, vertSlerpSpeed );
+        } else {
+            transform.rotation = Quaternion.Slerp( transform.rotation, noQuat, vertSlerpSpeed );
+        }
+
+        transform.Translate( Vector3.forward * speed * Time.deltaTime ); 
+        
         if ( transform.position.y > maxAltitude ) {
             Debug.Log("Leaving orbit");
             ExitScene();
