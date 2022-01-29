6875a8583a147cd852169002510ef09621a3b4e5
Marcus Maczynski <mmaczyns@asu.edu>
2022-01-28 22:47:35 -0800

    Adds intermediate level handling

--- Assets/Hub/Code/MainMenu.cs
+++ Assets/Hub/Code/MainMenu.cs
@@ -9,13 +9,30 @@ public class MainMenu : MonoBehaviour
 {
     int sceneNum;
     DialogGenerator diagIntro;
+    DialogGenerator diagFlight;
+    DialogGenerator diagScan;
     bool introStarted;
     // Start is called before the first frame update
     void Start()
     {
         diagIntro = GameObject.Find("DialogIntro").GetComponent<DialogGenerator>();
+        diagFlight = GameObject.Find("DialogFlight").GetComponent<DialogGenerator>();
+        diagScan = GameObject.Find("DialogScan").GetComponent<DialogGenerator>();
         introStarted = false;
         
+        switch (HubTracker.LevelToLoad)
+        {
+            case 2:
+                diagFlight.BeginPlayingDialog();
+                break;
+            case 3:
+                diagScan.BeginPlayingDialog();
+                break;
+            default:
+                break;
+
+        }
+
     }
 
     // Update is called once per frame
@@ -24,10 +41,18 @@ public class MainMenu : MonoBehaviour
         if (introStarted)
         {
             //if (diagIntro.AllDialogComplete())
-            if(diagIntro == null)
+            if(diagIntro == null && HubTracker.LevelToLoad == 1)
             {
                 //Scene scene = SceneManager.GetActiveScene();
                 //sceneNum = scene.buildIndex;
+                SceneManager.LoadScene(HubTracker.LevelToLoad++ + 1);
+            }
+            else if(diagFlight == null && HubTracker.LevelToLoad == 2)
+            {
+                SceneManager.LoadScene(HubTracker.LevelToLoad++ + 1);
+            }
+            else if (diagScan == null && HubTracker.LevelToLoad == 3)
+            {
                 SceneManager.LoadScene(HubTracker.LevelToLoad++ + 1);
             }
         }
