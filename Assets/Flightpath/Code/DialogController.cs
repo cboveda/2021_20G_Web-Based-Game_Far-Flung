80c8389260de47a2189dfe47b018f7a1dd716c44
Chris Boveda <cboveda@asu.edu>
2022-02-02 19:53:54 -0800

    Highlight relevant control for each stage of tutorial

--- Assets/Flightpath/Code/DialogController.cs
+++ Assets/Flightpath/Code/DialogController.cs
@@ -15,7 +15,19 @@ namespace Flightpath
         [SerializeField]
         private Slider _powerSlider;
         [SerializeField]
+        private GameObject _powerHandle;
+        [SerializeField]
+        private GameObject _powerTargetArrow;
+        [SerializeField]
+        private GameObject _powerTargetArrow2;
+        [SerializeField]
         private Slider _angleSlider;
+        [SerializeField]
+        private GameObject _angleHandle;
+        [SerializeField]
+        private GameObject _angleTargetArrow;
+        [SerializeField]
+        private GameObject _angleTargetArrow2;
         [SerializeField]
         private LaunchManager _launchManager;
 
@@ -30,6 +42,12 @@ namespace Flightpath
         private int _phase;
         private float _timeDelta;
         private float _timeMax;
+        [SerializeField]
+        private Color _highlightColor;
+        [SerializeField]
+        private Color _defaultColor;
+        [SerializeField]
+        private float _lerpRatio;
 
         // Start is called before the first frame update
         void Start()
@@ -38,6 +56,10 @@ namespace Flightpath
             _resetButton.interactable = false;
             _powerSlider.interactable = false;
             _angleSlider.interactable = false;
+            _angleTargetArrow.GetComponent<Image>().enabled = false;
+            _angleTargetArrow2.GetComponent<Image>().enabled = false;
+            _powerTargetArrow.GetComponent<Image>().enabled = false;
+            _powerTargetArrow2.GetComponent<Image>().enabled = false;
 
             _scriptIndex = 0;
             _scriptMax = Scripts.Length;
@@ -58,11 +80,22 @@ namespace Flightpath
             {
                 _dg.BeginPlayingDialog();
             }
-            _angleSlider.interactable = true;
+            if (_angleSlider.interactable == false)
+            {
+                _angleSlider.interactable = true;
+                _angleTargetArrow.GetComponent<Image>().enabled = true;
+                _angleTargetArrow2.GetComponent<Image>().enabled = true;
+            }
+            _angleTargetArrow.GetComponent<Image>().color = Color.Lerp(_defaultColor, _highlightColor, Mathf.PingPong(Time.time, _lerpRatio));
+            _angleTargetArrow2.GetComponent<Image>().color = Color.Lerp(_defaultColor, _highlightColor, Mathf.PingPong(Time.time, _lerpRatio));
+            _angleHandle.GetComponent<Image>().color = Color.Lerp(_defaultColor, _highlightColor, Mathf.PingPong(Time.time, _lerpRatio));
             float value = _angleSlider.GetComponent<Slider>().value;
             if (42 <= value && value <= 43)
             {
                 _angleSlider.interactable = false;
+                _angleTargetArrow.GetComponent<Image>().enabled = false;
+                _angleTargetArrow2.GetComponent<Image>().enabled = false;
+                _angleHandle.GetComponent<Image>().color = _defaultColor;
                 _phase++;
                 SetDG();
             }
@@ -74,11 +107,22 @@ namespace Flightpath
             {
                 _dg.BeginPlayingDialog();
             }
-            _powerSlider.interactable = true;
+            if (_powerSlider.interactable == false)
+            {
+                _powerSlider.interactable = true;
+                _powerTargetArrow.GetComponent<Image>().enabled = true;
+                _powerTargetArrow2.GetComponent<Image>().enabled = true;
+            }
+            _powerHandle.GetComponent<Image>().color = Color.Lerp(_defaultColor, _highlightColor, Mathf.PingPong(Time.time, _lerpRatio));
+            _powerTargetArrow.GetComponent<Image>().color = Color.Lerp(_defaultColor, _highlightColor, Mathf.PingPong(Time.time, _lerpRatio));
+            _powerTargetArrow2.GetComponent<Image>().color = Color.Lerp(_defaultColor, _highlightColor, Mathf.PingPong(Time.time, _lerpRatio));
             float value = _powerSlider.GetComponent<Slider>().value;
             if (84 <= value && value <= 86)
             {
                 _powerSlider.interactable = false;
+                _powerTargetArrow.GetComponent<Image>().enabled = false;
+                _powerTargetArrow2.GetComponent<Image>().enabled = false;
+                _powerHandle.GetComponent<Image>().color = _defaultColor;
                 _phase++;
                 SetDG();
             }
@@ -90,10 +134,15 @@ namespace Flightpath
             {
                 _dg.BeginPlayingDialog();
             }
-            _launchButton.interactable = true;
+            if (_launchButton.interactable == false)
+            {
+                _launchButton.interactable = true;
+            }
+            _launchButton.GetComponent<Image>().color = Color.Lerp(_defaultColor, _highlightColor, Mathf.PingPong(Time.time, _lerpRatio));
             if (_launchManager.hasStopped())
             {
                 _launchButton.interactable = false;
+                _launchButton.GetComponent<Image>().color = _defaultColor;
                 _phase++;
                 SetDG();
             }
@@ -117,9 +166,14 @@ namespace Flightpath
             {
                 _dg.BeginPlayingDialog();
             }
-            _resetButton.interactable = true;
+            if (_resetButton.interactable == false)
+            {
+                _resetButton.interactable = true;
+            }
+            _resetButton.GetComponent<Image>().color = Color.Lerp(_defaultColor, _highlightColor, Mathf.PingPong(Time.time, _lerpRatio));
             if (!_launchManager.hasStopped())
             {
+                _resetButton.GetComponent<Image>().color = _defaultColor;
                 _launchButton.interactable = true;
                 _angleSlider.interactable = true;
                 _powerSlider.interactable = true;
@@ -161,7 +215,7 @@ namespace Flightpath
             }
         }
 
-        void SetDG()
+        private void SetDG()
         {
             if (_scriptIndex < _scriptMax)
             {
@@ -171,5 +225,4 @@ namespace Flightpath
             }
         }
     }
-
 }
