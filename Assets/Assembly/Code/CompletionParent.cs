96e84234c804d715a3b139d818a00b9174e73dc9
amcarro3 <64031912+amcarro3@users.noreply.github.com>
2022-02-05 10:10:38 -0700

    Us403 (#106)
    * Task#458  - completed
    
    Make the assembly box puzzle solution a 3d vector
    
    * Task# 463 - Completed
    
    Change Assembly Completion to move to the hub
    
    * Task#473 - Completed
    
    Change the assembly pause to describe the game mechanic

--- Assets/Assembly/Code/CompletionParent.cs
+++ Assets/Assembly/Code/CompletionParent.cs
@@ -2,25 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class CompletionParent : MonoBehaviour, Completion
 {
-    public UnityEvent next;
-        public bool IsCompleted(){
+    public bool IsCompleted()
+    {
 
-        if(GetComponentInChildren<Completion>()==null){
+        if (GetComponentInChildren<Completion>() == null)
+        {
             Debug.Log("No Completion found");
         }
-        foreach(Completion completion in GetComponentsInChildren<Completion>()){
-            if((object) completion == this) continue;
+        foreach (Completion completion in GetComponentsInChildren<Completion>())
+        {
+            if ((object)completion == this) continue;
 
-            if(!completion.IsCompleted()) return false;
+            if (!completion.IsCompleted()) return false;
         }
         return true;
     }
 
-    public void OnCompletion(){
-        next.Invoke();
+    public void OnCompletion()
+    {
+        SceneManager.LoadScene("Hub");
     }
-   
+
 }
