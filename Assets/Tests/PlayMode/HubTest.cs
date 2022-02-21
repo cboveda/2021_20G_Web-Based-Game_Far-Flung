e0669e217307172749e9f51a18379f09fd3e1fd8
Marcus Maczynski <mmaczyns@asu.edu>
2022-02-21 12:41:41 -0800

    Add Hub TestRunner assembly and pieces

---
+++ Assets/Tests/PlayMode/HubTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using DialogMaker;
+
+public class HubTest
+{
+    [UnityTest]
+    public IEnumerator Test_HubTracker()
+    {
+        yield return new WaitForSeconds(0.1f);
+    }
+
+}
