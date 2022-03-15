1ba0d464d8d8e3c4444510885c2802db4c1a87a1
Chris Boveda <cboveda@asu.edu>
2022-03-15 09:36:09 -0700

    Fix dialog pool bug

--- Assets/Flightpath/Code/LaunchManager.cs
+++ Assets/Flightpath/Code/LaunchManager.cs
@@ -43,6 +43,9 @@ namespace Flightpath
         [SerializeField]
         private DialogScriptableObject[] _marsScripts;
         public DialogScriptableObject[] MarsScripts { get; set; }
+        [SerializeField]
+        private DialogScriptableObject[] _winScripts;
+        public DialogScriptableObject[] WinScripts { get; set; }
         private int _lastScript;
         private bool _sceneAdvanceStart;
         private bool _launchLocked;
@@ -146,6 +149,9 @@ namespace Flightpath
                     break;
                 case MarsBoundaryScriptIndex:
                     _dg.dialogContainer = pickNewScript(_marsScripts);
+                    break;
+                case WinBoundaryScriptIndex:
+                    _dg.dialogContainer = pickNewScript(_winScripts);
                     break;
                 default:
                     break;
