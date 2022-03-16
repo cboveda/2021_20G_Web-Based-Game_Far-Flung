f8623d554c3a1a7a9c13a8a7aac0f0ecfcdf646a
iswanlun <iswanlun@asu.edu>
2022-03-16 00:42:31 -0700

    psyche mockup built

--- Assets/Assembly/Code/ParentGhost.cs
+++ Assets/Assembly/Code/ParentGhost.cs
@@ -1,19 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ParentGhost : MonoBehaviour
-{
+public class ParentGhost : MonoBehaviour {
+    
     public float rotationSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        this.transform.Rotate(Vector3.down * (rotationSpeed * Time.deltaTime));
+    void Update() {
+
+        this.transform.Rotate(new Vector3(1, 1, 0) * (rotationSpeed * Time.deltaTime));
     }
 }
