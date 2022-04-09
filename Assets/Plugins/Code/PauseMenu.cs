5cc089363b39778911491cf5f4f1920940ba1e08
Chris Boveda <cboveda@asu.edu>
2022-04-08 22:43:19 -0700

    Add new pause menu to all scenes

---
+++ Assets/Plugins/Code/PauseMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private Button openMenuButton;
+    [SerializeField] private Button closeMenuButton;
+    [SerializeField] private Button returnToHubButton;
+    [SerializeField] private GameObject pauseMenuCanvas;
+    [SerializeField] private GameObject pauseMenuButtonCanvas;
+    private bool active;
+
+    void Start()
+    {
+        pauseMenuCanvas.SetActive(false);
+        pauseMenuButtonCanvas.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            toggleMenu();
+        }
+    }
+
+    public void toggleMenu()
+    {
+        pauseMenuCanvas.SetActive(!pauseMenuCanvas.active);
+    }
+
+    public void returnToHub()
+    {
+        SceneManager.LoadScene("Hub");
+    }
+}
