e11bbe8663d11b8de086d0bf3d39a3847a4277cf
Chris Boveda <cboveda@asu.edu>
2022-04-09 14:48:08 -0700

    Add instructions to pause menu based on active game.

--- Assets/Plugins/Code/PauseMenu.cs
+++ Assets/Plugins/Code/PauseMenu.cs
@@ -10,17 +10,62 @@ using UnityEngine.SceneManagement;
 */
 public class PauseMenu : MonoBehaviour
 {
+    private const int ASSY = 0;
+    private const int FLIGHT = 1;
+    private const int SCAN = 2;
+    private const int COM = 3;
+    private const int LAB = 4;
+    [SerializeField] private GameObject[] instructionSet;
     [SerializeField] private Button openMenuButton;
     [SerializeField] private Button closeMenuButton;
     [SerializeField] private Button returnToHubButton;
     [SerializeField] private GameObject pauseMenuCanvas;
     [SerializeField] private GameObject pauseMenuButtonCanvas;
-    private bool active;
 
     void Start()
     {
         pauseMenuCanvas.SetActive(false);
         pauseMenuButtonCanvas.SetActive(true);
+        foreach (GameObject g in instructionSet)
+        {
+            g.SetActive(false);
+        }
+        int target = getActiveInstructionSet();
+        if (0 <= target && target < instructionSet.Length)
+        {
+            instructionSet[target].SetActive(true);
+        }
+    }
+
+    private static int getActiveInstructionSet()
+    {
+        int target;
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentIndex == 1) //Assembly
+        {
+            target = ASSY;
+        }
+        else if (2 <= currentIndex && currentIndex <= 4) //Flight
+        {
+            target = FLIGHT;
+        }
+        else if (5 <= currentIndex && currentIndex <= 7) //Scanning
+        {
+            target = SCAN;
+        }
+        else if (8 <= currentIndex && currentIndex <= 13) //Com
+        {
+            target = COM;
+        }
+        else if (currentIndex == 14) //Lab
+        {
+            target = LAB;
+        }
+        else
+        {
+            target = -1;
+        }
+        return target;
     }
 
     void Update()
